fb10f9a81d020b4cd8f96f43586d856f143bf33b
apexa199 <apexaajudiya9@gmail.com>
2024-02-14 17:16:51 +0530

    Button and Popup

--- src/app/components/recruiter/listJobs.jsx
+++ src/app/components/recruiter/listJobs.jsx
@@ -1,19 +1,46 @@
 import * as React from 'react';
-import { Box, Chip, Grid, IconButton, InputAdornment, Rating, TextField } from '@mui/material';
+import {
+  Box,
+  CardActions,
+  Chip,
+  Grid,
+  IconButton,
+  InputAdornment,
+  Paper,
+  Rating,
+  Button,
+  TextField,
+  useMediaQuery
+} from '@mui/material';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
 import { Breadcrumb } from 'app/components';
 import Card from '@mui/material/Card';
 import styled from '@emotion/styled';
 import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
-import { CardActionArea } from '@mui/material';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { GetJobRequest } from 'slice/recruiter/getjobSlice';
 import SearchIcon from '@mui/icons-material/Search';
 import FilterListIcon from '@mui/icons-material/FilterList';
 import { useState } from 'react';
-import { Button } from 'react-bootstrap';
+import { useTheme } from '@emotion/react';
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4
+};
 
 const Container = styled('div')(({ theme }) => ({
   margin: '30px',
@@ -25,33 +52,6 @@ const Container = styled('div')(({ theme }) => ({
 }));
 
 function MyJobs() {
-  const [filterOpen, setFilterOpen] = useState(false);
-
-  const [searchOptions, setSearchOptions] = useState({
-    query: '',
-    jobType: {
-      fullTime: false,
-      partTime: false,
-      wfh: false
-    },
-    salary: [0, 100],
-    duration: '0',
-    sort: {
-      salary: {
-        status: false,
-        desc: false
-      },
-      duration: {
-        status: false,
-        desc: false
-      },
-      rating: {
-        status: false,
-        desc: false
-      }
-    }
-  });
-
   const { listData, data, error, isloading } = useSelector((y) => y.getJobs);
 
   console.log(listData);
@@ -62,6 +62,15 @@ function MyJobs() {
     dis(GetJobRequest(1));
   }, []);
 
+  const [open, setOpen] = useState(false);
+  const handleClickOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  const [openUpdate, setOpenUpdate] = useState(false);
+  const handleClickOpenUpdate = () => setOpenUpdate(true);
+  const handleCloseUpdate = () => setOpenUpdate(false);
+  const theme = useTheme();
+  const fullScreen = useMediaQuery(theme.breakpoints.down('md'));
   return (
     <>
       <Container>
@@ -79,17 +88,6 @@ function MyJobs() {
             <Grid item xs>
               <TextField
                 label="Search Jobs"
-                value={searchOptions.query}
-                onChange={(event) =>
-                  setSearchOptions({
-                    ...searchOptions,
-                    query: event.target.value
-                  })
-                }
-                onKeyPress={(ev) => {
-                  if (ev.key === 'Enter') {
-                  }
-                }}
                 InputProps={{
                   endAdornment: (
                     <InputAdornment>
@@ -104,7 +102,7 @@ function MyJobs() {
               />
             </Grid>
             <Grid item>
-              <IconButton onClick={() => setFilterOpen(true)}>
+              <IconButton>
                 <FilterListIcon />
               </IconButton>
             </Grid>
@@ -114,18 +112,18 @@ function MyJobs() {
             listData?.map((v) => {
               return (
                 <Card sx={{ maxWidth: 1200, margin: '25px', background: 'lightgrey' }}>
-                  <CardActionArea>
+                  <div style={{ display: 'flex', justifyContent: 'space-between' }}>
                     <CardContent>
                       <Typography gutterBottom variant="h6" component="div">
                         {v.title}
                       </Typography>
+
                       <Typography gutterBottom variant="h6" component="div">
                         <Rating value={v.rating !== -1 ? v.rating : null} readOnly />
                       </Typography>
                       <Typography gutterBottom variant="body" component="div">
                         User Id : {v.userId}
                       </Typography>
-
                       <Typography gutterBottom variant="body" component="div">
                         Duration :{v.duration !== 0 ? `${v.duration} month` : `Flexible`}
                       </Typography>
@@ -150,7 +148,54 @@ function MyJobs() {
                         ))}
                       </Typography>
                     </CardContent>
-                  </CardActionArea>
+
+                    {/* Button  code start--------------------------> */}
+
+                    <CardActions style={{ display: 'grid' }}>
+                      <Button
+                        style={{
+                          backgroundColor: 'rgb(34 42 68)',
+                          color: 'white',
+                          padding: '69px 118px',
+                          borderRadius: '7px',
+                          border: 'none'
+                        }}
+                        size="small"
+                        onClick={() => handleClick(`/recruiter/applications`)}
+                      >
+                        View Application
+                      </Button>
+                      <Button
+                        style={{
+                          backgroundColor: 'rgb(252, 143, 30)',
+                          color: 'white',
+                          margin: '0px 0px',
+                          padding: '10px 15px',
+                          borderRadius: '7px',
+                          border: 'none'
+                        }}
+                        size="small"
+                        onClick={handleClickOpenUpdate}
+                      >
+                        Update Details
+                      </Button>
+                      <Button
+                        style={{
+                          backgroundColor: '#f50057',
+                          color: 'white',
+                          margin: '0px 0px',
+                          padding: '10px 15px',
+                          borderRadius: '7px',
+                          border: 'none'
+                        }}
+                        size="small"
+                        onClick={handleClickOpen}
+                      >
+                        Delete
+                      </Button>
+                    </CardActions>
+                    {/* Button  code end----------------------------> */}
+                  </div>
                 </Card>
               );
             })
@@ -160,6 +205,119 @@ function MyJobs() {
             </Typography>
           )}
         </div>
+        {/*Delete Pop Up-----------------> */}
+
+        <Dialog
+          open={open}
+          keepMounted
+          onClose={handleClose}
+          aria-describedby="alert-dialog-slide-description"
+        >
+          <DialogTitle variant="h4" align="center">
+            Are You Sure?
+          </DialogTitle>
+          <DialogActions>
+            <Button
+              style={{
+                backgroundColor: '#f50057',
+                color: 'white',
+                margin: '20px',
+                padding: '11px 60px',
+                borderRadius: '7px',
+                border: 'none'
+              }}
+              size="small"
+              onClick={handleClose}
+            >
+              DELETE
+            </Button>
+            <Button
+              style={{
+                backgroundColor: 'rgb(34 42 68)',
+                color: 'white',
+                margin: '0px 0px',
+                padding: '11px 60px',
+                borderRadius: '7px',
+                border: 'none'
+              }}
+              size="small"
+              onClick={handleClose}
+            >
+              CANCEL
+            </Button>
+          </DialogActions>
+        </Dialog>
+
+        {/* /delete popup end-------------> */}
+
+        {/* /Update Popup Start----------> */}
+        <Dialog
+          fullScreen={fullScreen}
+          open={openUpdate}
+          keepMounted
+          onClose={handleCloseUpdate}
+          aria-describedby="alert-dialog-slide-description"
+          style={{ padding: '0px 24px' }}
+        >
+          <DialogTitle variant="h4" align="center" style={{ padding: '35px 157px' }}>
+            Update Details
+          </DialogTitle>
+          <TextField
+            label="Application Deadline"
+            type="datetime-local"
+            InputLabelProps={{
+              shrink: true
+            }}
+            variant="outlined"
+            style={{ marginBottom: '10px' }}
+          />
+          <TextField
+            label="Maximum Number Of Applicants"
+            type="number"
+            variant="outlined"
+            InputProps={{ inputProps: { min: 1 } }}
+            style={{ marginBottom: '10px' }}
+          />
+          <TextField
+            label="Positions Available"
+            type="number"
+            variant="outlined"
+            InputProps={{ inputProps: { min: 1 } }}
+            style={{ marginBottom: '10px' }}
+          />
+
+          <DialogActions style={{ display: 'flex', justifyContent: 'space-around' }}>
+            <Button
+              style={{
+                backgroundColor: '#f50057',
+                color: 'white',
+                margin: '19px',
+                padding: '11px 60px',
+                borderRadius: '7px',
+                border: 'none'
+              }}
+              size="small"
+              onClick={handleCloseUpdate}
+            >
+              UPDATE
+            </Button>
+            <Button
+              style={{
+                backgroundColor: 'rgb(34 42 68)',
+                color: 'white',
+                margin: '0px 0px',
+                padding: '11px 60px',
+                borderRadius: '7px',
+                border: 'none'
+              }}
+              size="small"
+              onClick={handleCloseUpdate}
+            >
+              CANCEL
+            </Button>
+          </DialogActions>
+        </Dialog>
+        {/* Update Popup End------------------> */}
       </Container>
     </>
   );
