72226225ed1689a975ece82cc6b7776aaffb565d
apexa199 <apexaajudiya9@gmail.com>
2024-02-15 16:59:11 +0530

    p

--- src/app/navigations.js
+++ src/app/navigations.js
@@ -1,9 +1,9 @@
 export const navigations = [
   { name: 'Dashboard', path: '/dashboard/default', icon: 'dashboard' },  
   { name: 'Recruiter', path: '/recruiter/default',icon: 'dashboard' },  
-  { name: 'CreateJob', path: '/recruiter/createjob',icon: 'dashboard' },
-  { name: 'MyJobs', path: '/recruiter/myjobs', icon: 'dashboard'  },
-  { name: 'Applications', path: '/recruiter/applications', icon: 'dashboard'  },
+  { name: 'CreateJob', path: '/recruiter/createjob',icon: 'create' },
+  { name: 'MyJobs', path: '/recruiter/myjobs', icon: 'assignment'  },
+  
 
  
  
