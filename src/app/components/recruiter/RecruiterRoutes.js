495cc22fd2504e28a0a181566b938d7a6df77105
apexa199 <apexaajudiya9@gmail.com>
2024-02-15 14:15:21 +0530

    p

--- src/app/components/recruiter/RecruiterRoutes.js
+++ src/app/components/recruiter/RecruiterRoutes.js
@@ -2,13 +2,13 @@ import { lazy } from 'react';
 import Loadable from 'app/components/Loadable';
 import { CreateJob } from './createJob';
 import Profile from './profile';
-import ListJobs from './listJobs';
+import MyJobs from './MyJobs';
 
 
 const recruiterRoutes = [
   { path: '/recruiter/createJob', element: <CreateJob /> },
   { path: '/recruiter/profile', element: <Profile /> },
-  { path: '/recruiter/listJobs', element: <ListJobs /> },
+  { path: '/recruiter/MyJobs', element: <MyJobs /> },
 
 ];
 
--- src/app/navigations.js
+++ src/app/navigations.js
@@ -2,7 +2,7 @@ export const navigations = [
   { name: 'Dashboard', path: '/dashboard/default', icon: 'dashboard' },  
   { name: 'Recruiter', path: '/recruiter/default',icon: 'dashboard' },  
   { name: 'CreateJob', path: '/recruiter/createjob',icon: 'dashboard' },
-  { name: 'ListJob', path: '/recruiter/listjobs', icon: 'dashboard'  },
+  { name: 'MyJobs', path: '/recruiter/myjobs', icon: 'dashboard'  },
   { name: 'Applications', path: '/recruiter/applications', icon: 'dashboard'  },
 
  
