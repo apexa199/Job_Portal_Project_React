8064bd8dae64ad1804ad368dc360b68ed01ca0a0
apexa199 <apexaajudiya9@gmail.com>
2024-02-20 15:09:31 +0530

    signup register for applicant

---
+++ src/app/views/sessions/JwtRegisterForApplicant.jsx
@@ -0,0 +1,272 @@
+import React from 'react'
+import { useTheme } from '@emotion/react';
+import { LoadingButton } from '@mui/lab';
+import { Card, Checkbox, Grid, TextField } from '@mui/material';
+import { Box, styled } from '@mui/material';
+import { Paragraph } from 'app/components/Typography';
+import useAuth from 'app/hooks/useAuth';
+import { Formik } from 'formik';
+import { useState } from 'react';
+import { NavLink, useNavigate } from 'react-router-dom';
+import * as Yup from 'yup';
+import Button from '@mui/material/Button';
+import ButtonGroup from '@mui/material/ButtonGroup';
+import DescriptionIcon from '@mui/icons-material/Description';
+import AccountBoxIcon from '@mui/icons-material/AccountBox';
+import CloudUploadIcon from '@mui/icons-material/CloudUpload';
+
+const VisuallyHiddenInput = styled('input')({
+    clip: 'rect(0 0 0 0)',
+    clipPath: 'inset(50%)',
+    height: 1,
+    overflow: 'hidden',
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    whiteSpace: 'nowrap',
+    width: 1,
+  });
+  
+const FlexBox = styled(Box)(() => ({ display: 'flex', alignItems: 'center' }));
+
+const JustifyBox = styled(FlexBox)(() => ({ justifyContent: 'center' }));
+
+const ContentBox = styled(JustifyBox)(() => ({
+  height: '100%',
+  padding: '32px',
+  background: 'rgba(0, 0, 0, 0.01)'
+}));
+
+const JWTRegisterForApplicant = styled(JustifyBox)(() => ({
+  background: '#1A2038',
+  minHeight: '100vh !important',
+  '& .card': {
+    maxWidth: 800,
+    minHeight: 400,
+    margin: '1rem',
+    display: 'flex',
+    borderRadius: 12,
+    alignItems: 'center'
+  }
+}));
+
+// inital login credentials
+const initialValues = {
+    type: "applicant",
+    email: "",
+    password: "",
+    name: "",
+    education: [],
+    skills: [],
+    resume: "",
+    profile: "",
+    bio: "",
+    contactNumber: "",
+};
+
+// form field validation schema
+const validationSchema = Yup.object().shape({
+  password: Yup.string()
+    .min(6, 'Password must be 6 character length')
+    .required('Password is required!'),
+  email: Yup.string().email('Invalid Email address').required('Email is required!')
+});
+
+
+
+export const JwtRegisterForApplicant = () => {
+
+    const theme = useTheme();
+    const { register } = useAuth();
+    const navigate = useNavigate();
+    const [loading, setLoading] = useState(false);
+  
+    const handleFormSubmit = (values) => {
+      alert('r');
+      setLoading(true);
+    }
+  return (
+    <JWTRegisterForApplicant>
+    <Card className="card">
+      <Grid container>
+        <Grid item sm={6} xs={12}>
+          <ContentBox>
+            <img
+              width="100%"
+              alt="Register"
+              src="/assets/images/illustrations/posting_photo.svg"
+            />
+          </ContentBox>
+        </Grid>
+
+        <Grid item sm={6} xs={12}>
+          <Box p={4} height="100%">
+            <Formik
+              onSubmit={handleFormSubmit}
+              initialValues={initialValues}
+              validationSchema={validationSchema}
+            >
+              {({ values, errors, touched, handleChange, handleBlur, handleSubmit }) => (
+                <form onSubmit={handleSubmit}>
+                  <TextField
+                    fullWidth
+                    size="small"
+                    type="text"
+                    name="name"
+                    label="Name"
+                    variant="outlined"
+                    onBlur={handleBlur}
+                    value={values.name}
+                    onChange={handleChange}
+                    helperText={touched.name && errors.name}
+                    error={Boolean(errors.name && touched.name)}
+                    sx={{ mb: 3 }}
+                  />
+
+                  <TextField
+                    fullWidth
+                    size="small"
+                    type="email"
+                    name="email"
+                    label="Email"
+                    variant="outlined"
+                    onBlur={handleBlur}
+                    value={values.email}
+                    onChange={handleChange}
+                    helperText={touched.email && errors.email}
+                    error={Boolean(errors.email && touched.email)}
+                    sx={{ mb: 3 }}
+                  />
+                  <TextField
+                    fullWidth
+                    size="small"
+                    name="password"
+                    type="password"
+                    label="Password"
+                    variant="outlined"
+                    onBlur={handleBlur}
+                    value={values.password}
+                    onChange={handleChange}
+                    helperText={touched.password && errors.password}
+                    error={Boolean(errors.password && touched.password)}
+                    sx={{ mb: 2 }}
+                  />
+
+                <TextField
+                   fullWidth
+                   size="small"
+                   type="text"
+                   name="skills"
+                   label="Skills"
+                   variant="outlined"
+                   onBlur={handleBlur}
+                   value={values.skills}
+                   onChange={handleChange}
+                   helperText={touched.skills && errors.skills}
+                   error={Boolean(errors.skills && touched.skills)}
+                   sx={{ mb: 3 }}
+                 /> 
+                 
+                <ButtonGroup  fullWidth variant="outlined" aria-label="Basic button group" sx={{ mb: 3 }}>
+                    <Button
+                        component="label"
+                        role={undefined}
+                        variant="contained"
+                        tabIndex={-1}
+                        style={{width : "40%"}}
+                        startIcon={<DescriptionIcon />}
+                        >
+                        <VisuallyHiddenInput type="file" />
+                        </Button>
+
+                        <Button style={{width : "221%",color : "#c4c4c4", borderColor:"#c4c4c4"}}>
+                        Resume (.pdf)              
+                    
+                    </Button>
+                        <Button
+                            component="label"
+                            role={undefined}
+                            disabled
+                            style={{width : "40%"}}
+                            variant="contained"
+                            tabIndex={-1}
+                            startIcon={<CloudUploadIcon  />}
+                            >                
+                            
+                        </Button>
+                </ButtonGroup>
+                 
+                <ButtonGroup  fullWidth variant="outlined" aria-label="Basic button group"   sx={{ mb: 3 }}>
+                    <Button
+                        component="label"
+                        role={undefined}
+                        variant="contained"
+                        tabIndex={-1}
+                        style={{width : "40%"}}
+                        startIcon={<AccountBoxIcon />}
+                        >
+                        <VisuallyHiddenInput type="file" />
+                        </Button>
+
+                        <Button style={{width : "221%", color : "#c4c4c4", borderColor:"#c4c4c4"}}>
+                        Profile Photo (.jpg/.png)        
+                    
+                    </Button>
+                        <Button
+                            component="label"
+                            role={undefined}
+                            disabled
+                            style={{width : "40%"}}
+                            variant="contained"
+                            tabIndex={-1}
+                            startIcon={<CloudUploadIcon  />}
+                            >                
+                            
+                        </Button>
+                </ButtonGroup>
+                 
+
+                  <FlexBox gap={1} alignItems="center">
+                    <Checkbox
+                      size="small"
+                      name="remember"
+                      onChange={handleChange}
+                      checked={values.remember}
+                      sx={{ padding: 0 }}
+                    />
+
+                    <Paragraph fontSize={13}>
+                      I have read and agree to the terms of service.
+                    </Paragraph>
+                  </FlexBox>
+
+                  <LoadingButton
+                    type="submit"
+                    color="primary"
+                    loading={loading}
+                    variant="contained"
+                    sx={{ mb: 2, mt: 3 }}
+                  >
+                    Regiser
+                  </LoadingButton>
+
+                  <Paragraph>
+                    Already have an account?
+                    <NavLink
+                      to="/session/signin"
+                      style={{ color: theme.palette.primary.main, marginLeft: 5 }}
+                    >
+                      Login
+                    </NavLink>
+                  </Paragraph>
+                </form>
+              )}
+            </Formik>
+          </Box>
+        </Grid>
+      </Grid>
+    </Card>
+  </JWTRegisterForApplicant>
+);
+};
+
